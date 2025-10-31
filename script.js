6695377db61cdd77f65a8db710dcd4ae5d4f211c
kermani65156515 <kermani6515@gmail.com>
2025-11-01 01:44:12 +0330

    first V

---
+++ script.js
@@ -0,0 +1,14 @@
+// گرفتن المان‌های HTML
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+// تابع برای اضافه کردن وظیفه
+function addTask(e) {
+    e.preventDefault();
+
+    const taskText = taskInput.value.trim();
+
+    function sum(a,b) {
+        ``
+    }
